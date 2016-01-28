daa7e9da6d0025345c31fb9fa96fca63850ef32e
Herman Moreno <herman.moreno@crowdint.com>
2016-01-28 12:57:32 -0600

    Change delegation to follow new strategy

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Product.class_eval do
-  delegate :sale_price, to: :master
+  delegate :sale_price, :sale_price=, to: :find_or_build_master
 
   def on_sale?
     variants_including_master.any?{ |v| v.on_sale? }
